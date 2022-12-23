9d86747e63dce7e4cdf4381014c79eaebd796159
vitax10 <113950146+vitax10@users.noreply.github.com>
2022-12-24 00:57:23 +0100

    kohv04-SP2

---
+++ www/kohv04/SP2/map.js
@@ -0,0 +1,5 @@
+const map = L.map('map').setView([50, 15], 5);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
