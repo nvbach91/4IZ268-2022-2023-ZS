811d356743e31c86b26156ee9a6eab42c53a60cb
jakubrousal <114013170+jakubrousal@users.noreply.github.com>
2022-12-23 22:18:21 +0100

    rouj07-sp2
    https://esotemp.vse.cz/~rouj07/sp2/

---
+++ www/rouj07/sp2/js/main.js
@@ -0,0 +1,13 @@
+$(document).ready(() => {
+
+    // Send all data requests to:
+    // http://www.omdbapi.com/?i=tt3896198&apikey=70e522f5
+
+    // function getMovies(searchText) {
+    //     axios.get(`http://www.omdbapi.com/?i=tt3896198&apikey=70e522f5=${searchText}`).then((resp) => {
+    //         const movies = resp.data.Search;
+    //         let output = "";
+    //     })
+    // }
+
+});
