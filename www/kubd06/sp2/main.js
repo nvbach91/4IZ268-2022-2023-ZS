9dc229f9fcb8e6c9952850b28a488bc313f21e90
0.00023 <114229711+nuladvacettrojka@users.noreply.github.com>
2022-12-24 14:59:28 +0100

    kubd06-sp2
    Link to application: https://esotemp.vse.cz/~kubd06/sp2/

---
+++ www/kubd06/sp2/main.js
@@ -0,0 +1,12 @@
+console.log('cs_buz');
+(() => {
+    const createQuestion = () =>{
+
+    }
+    const submitButton = document.createElement("button");
+    submitButton.innerText = "poslechnout";
+    document.body.appendChild(submitButton);
+    const controlAnswer = () => {
+
+    }
+})()
