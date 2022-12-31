d2d6df1c4b83264e3ce7aac79d30c2000d24974b
Alena Kalivodová <94915126+ShiriiK@users.noreply.github.com>
2022-12-31 12:34:37 +0100

    kala13-sp2

---
+++ www/kala13/sp2/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Header from './components/Header'
+
+function App() {
+  return (
+    <div className="container">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
