afe32139986895e397bed3d637d6dfe25c2e24b2
matyaskohout <82026714+matyaskohout@users.noreply.github.com>
2023-01-24 17:54:41 +0100

    oprava sp2
    + náprava připomínek k sp2
    + záznam uživatel provádějícího změnu
    + datum úpravy

--- www/kohm09/sp2/js/script.js
+++ www/kohm09/sp2/js/script.js
@@ -14,11 +14,14 @@ const DISCOVERY_DOC = "https://sheets.googleapis.com/$discovery/rest?version=v4"
 
 // included, separated by spaces.
 
-const SCOPES = "https://www.googleapis.com/auth/spreadsheets";
+// Scopes for spreadsheets + profile
+const SCOPES = "https://www.googleapis.com/auth/spreadsheets profile";
 
 let tokenClient;
 
 let tableData;
+
+let userEmail = ''
 
 let editingRow = -1;
 
@@ -28,9 +31,31 @@ let gisInited = false;
 
 let numberOfRows = 0;
 
-document.getElementById("authorize_button").style.visibility = "visible";
+const spinner = document.getElementById("spinner");
+const errMess = document.getElementById("errMess");
+const write_button = document.getElementById("write_button");
+const finish_edit_button = document.getElementById("finish_edit_button");
+const authorize_button = document.getElementById("authorize_button");
+const load_sheets = document.getElementById("load_sheets");
 
-document.getElementById("signout_button").style.visibility = "hidden";
+const nameInputElement = document.getElementById("nameInput");
+const surnameInputElement = document.getElementById("surnameInput");
+const yearInputElement = document.getElementById("yearInput");
+const weightInputElement = document.getElementById("weightInput");
+const heightInputElement = document.getElementById("heightInput");
+
+
+
+write_button.onclick = () => handleWrite();
+finish_edit_button.onclick = () => handleWrite();
+load_sheets.onclick = () => load();
+authorize_button.onclick = () => handleAuthClick();
+
+showLastLogin();
+
+
+authorize_button.style.visibility = "visible";
+
 
 /**
 
@@ -57,22 +82,71 @@ async function initializeGapiClient() {
     discoveryDocs: [DISCOVERY_DOC],
   });
 
-  gapiInited = true;
 
+  // Load oauth2 module
+  await gapi.client.load('oauth2', 'v2');
+
+  gapiInited = true;
 }
 
 /**
  * Callback after Google Identity Services are loaded.
  */
 function gisLoaded() {
+
   tokenClient = google.accounts.oauth2.initTokenClient({
     client_id: CLIENT_ID,
-    scope: SCOPES, 
-    callback: "", 
+    scope: SCOPES,
+    callback: (resp) => {
+      console.log("RESP: ", resp)
+    },
   });
+
+
   gisInited = true;
+}
+
+/**
+ *  Sign in the user upon button click.
+ */
+function handleAuthClick() {
+  tokenClient.callback = async (resp) => {
+    if (resp.error !== undefined) {
+      throw resp;
+    }
+    authorize_button.style.visibility = "hidden";
+    load_sheets.style.visibility = "hidden";
+
+    gapi.client.oauth2.userinfo.get().execute(function (resp) {
+      console.log("RESPONSE: ", resp)
+      userEmail = resp.email;
+    })
+
+    load();
+    lastApearance();
+    showLastLogin();
+  };
+
+  if (gapi.client.getToken() === null) {
+    // Prompt the user to select a Google Account and ask for consent to share their data
+    // when establishing a new session.
+    tokenClient.requestAccessToken({ prompt: "consent" });
+  } else {
+    // Skip display of account chooser and consent dialog for an existing session.
+    tokenClient.requestAccessToken({ prompt: "" });
+  }
 
 }
+
+function lastApearance() {
+  localStorage.setItem('lastLogin', new Date().toLocaleString());
+}
+
+function showLastLogin() {
+  document.getElementById("lastLogin").innerText = `Poslední přihlášení: ${localStorage.getItem('lastLogin')}`;
+}
+
+
 
 async function handleDelete(rowNumberToDelete) {
   try {
@@ -108,24 +182,28 @@ async function handleDelete(rowNumberToDelete) {
 function resetInputs(rowNumberToUpdate) {
   editingRow = rowNumberToUpdate;
 
-  document.getElementById("finish_edit_button").style.visibility = "visible";
+  finish_edit_button.style.visibility = "visible";
 
-  document.getElementById("jmenoInput").value = tableData[rowNumberToUpdate - 1][0];
-  document.getElementById("prijmeniInput").value = tableData[rowNumberToUpdate - 1][1];
-  document.getElementById("datumNarozeniInput").value = tableData[rowNumberToUpdate - 1][2];
-  document.getElementById("vahaInput").value = tableData[rowNumberToUpdate - 1][3];
-  document.getElementById("vyskaInput").value = tableData[rowNumberToUpdate - 1][4];
+  write_button.style.visibility = "hidden";
+
+  nameInputElement.value = tableData[rowNumberToUpdate - 1][0];
+  surnameInputElement.value = tableData[rowNumberToUpdate - 1][1];
+  yearInputElement.value = tableData[rowNumberToUpdate - 1][2];
+  weightInputElement.value = tableData[rowNumberToUpdate - 1][3];
+  heightInputElement.value = tableData[rowNumberToUpdate - 1][4];
 }
 
 async function handleWrite(rowNumberToUpdate) {
-  const jmeno = document.getElementById("jmenoInput").value;
-  const prijmeni = document.getElementById("prijmeniInput").value;
-  const datumNarozeni = document.getElementById("datumNarozeniInput").value;
-  const vaha = document.getElementById("vahaInput").value;
-  const vyska = document.getElementById("vyskaInput").value;
+  const name = nameInputElement.value;
+  const surname = surnameInputElement.value;
+  const year = yearInputElement.value;
+  const weight = weightInputElement.value;
+  const height = heightInputElement.value;
+  const time = new Date().toLocaleString();
+  const user = userEmail;
 
   try {
-    const range = `DATA!A${editingRow !== -1 ? editingRow : numberOfRows + 1}:E${editingRow !== -1 ? editingRow : numberOfRows + 1
+    const range = `DATA!A${editingRow !== -1 ? editingRow : numberOfRows + 1}:G${editingRow !== -1 ? editingRow : numberOfRows + 1
       }`;
 
     const response = await gapi.client.sheets.spreadsheets.values.update({
@@ -134,70 +212,56 @@ async function handleWrite(rowNumberToUpdate) {
       range,
       resource: {
         range,
-        values: [[jmeno, prijmeni, datumNarozeni, vaha, vyska]],
+        values: [[name, surname, year, weight, height, time, user]],
       },
     });
     console.log("write response: ", response);
 
     editingRow = -1;
 
+    nameInputElement.value = "";
+    surnameInputElement.value = "";
+    yearInputElement.value = "";
+    weightInputElement.value = "";
+    heightInputElement.value = "";
 
-    document.getElementById("jmenoInput").value = "";
-    document.getElementById("prijmeniInput").value = "";
-    document.getElementById("datumNarozeniInput").value = "";
-    document.getElementById("vahaInput").value = "";
-    document.getElementById("vyskaInput").value = "";
-
-
-    document.getElementById("finish_edit_button").style.visibility = "hidden";
+    finish_edit_button.style.visibility = "hidden";
+    write_button.style.visibility = "visible";
 
     load();
-
   } catch (err) {
     console.log("error when writing : ", err);
 
-    document.getElementById("content").innerText === err.message;
+    errMess.innerText === err.message;
 
     return;
   }
 }
 
-/**
- *  Sign in the user upon button click.
- */
-function handleAuthClick() {
-  tokenClient.callback = async (resp) => {
-    if (resp.error !== undefined) {
-      throw resp;
-    }
-    document.getElementById("signout_button").style.visibility = "visible";
-    // document.getElementById("authorize_button").innerText = "Refresh";
-  };
-
-  if (gapi.client.getToken() === null) {
-    // Prompt the user to select a Google Account and ask for consent to share their data
-    // when establishing a new session.
-    tokenClient.requestAccessToken({ prompt: "consent" });
-  } else {
-    // Skip display of account chooser and consent dialog for an existing session.
-    tokenClient.requestAccessToken({ prompt: "" });
-  }
-  load();
-}
-
-
 async function load() {
   let response;
 
-  try {
+  spinner.style.visibility = "visible";
 
+  finish_edit_button.style.visibility = "hidden";
+  write_button.style.visibility = "visible";
+
+  nameInputElement.value = "";
+  surnameInputElement.value = "";
+  yearInputElement.value = "";
+  weightInputElement.value = "";
+  heightInputElement.value = "";
+
+  try {
     response = await gapi.client.sheets.spreadsheets.values.get({
       spreadsheetId: SPREADSHEET_ID,
       range: "Data!A:E",
     });
   } catch (err) {
     console.log("error when fetching: ", err);
-    document.getElementById("content").innerText = err.message;
+    errMess.innerText = err.message;
+
+    spinner.style.visibility = "hidden";
 
     return;
   }
@@ -206,7 +270,7 @@ async function load() {
   console.log("result: ", result);
 
   if (!result || !result.values || result.values.length == 0) {
-    document.getElementById("content").innerText = "No values found.";
+    errMess.innerText = "No values found.";
 
     return;
   }
@@ -217,20 +281,23 @@ async function load() {
 
   console.log(`${result.values.length} rows retrieved.`);
 
-
-
   const text = result.values
     .map(
       (row, idx) =>
         `<div class="dataRadek">${row
           .map((cell) => `<div class="dataZaznamy"  >${cell}</div>`)
-          .join(
-            ""
-          )}<div calss="dataZaznamy2"><button class="upr" onclick="resetInputs(${idx + 1
-        })">Upravit</button><button class="smz" id="radekCislo${idx + 1}" onclick="handleDelete(${idx + 1
-        })" >Smazat</button></div></div>`
+          .join("")}<div calss="dataZaznamy2"><button id="editButton${idx + 1
+        }" class="upr">Upravit</button><button id="deleteButton${idx + 1
+        }" class="smz" id="radekCislo${idx + 1}" >Smazat</button></div></div>`
     )
     .join("\n");
 
   document.getElementById("table").innerHTML = text;
+
+  spinner.style.visibility = "hidden";
+
+  result.values.forEach((row, idx) => {
+    document.getElementById(`deleteButton${idx + 1}`).onclick = () => handleDelete(idx + 1);
+    document.getElementById(`editButton${idx + 1}`).onclick = () => resetInputs(idx + 1);
+  });
 }
