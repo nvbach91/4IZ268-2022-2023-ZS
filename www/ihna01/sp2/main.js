47c3f8a7f5757385a5140c349f6e6bb2ba1cba7d
alinaihn <113994030+alinaihn@users.noreply.github.com>
2023-01-27 00:27:35 +0100

    version 03

--- www/ihna01/sp2/main.js
+++ www/ihna01/sp2/main.js
@@ -1,69 +1,56 @@
-const form = $("#search_form")
-const input = $("#search_term")
+const form = $("#search_form");
+const input = $("#search_term");
 
-const msg = document.querySelector(".form_msg")
-const list = document.querySelector(".cities")
+const msg = document.querySelector(".form_msg");
+const list = document.querySelector(".cities");
 
+const APIkey = 'c7259667067245b1c204f4af8fe5c5aa';
 
-const APIkey = 'c7259667067245b1c204f4af8fe5c5aa'
+//========
+ if (navigator.geolocation) {
+     navigator.geolocation.getCurrentPosition((position) => {
+         addCityToTheBoard(null, true, position.coords)
+     });
+ } 
+//========
 
+const initialWeather = localStorage.getItem('weather');
+if (initialWeather) {
+    initialWeather.split(',').forEach(city => {
+        addCityToTheBoard(city, true);
+    })
+}
 
-form.submit(e => {
-    e.preventDefault()
+function checkIfCityIsAdded(cityName) {
+    const weatherData = localStorage.getItem('weather') ;
+    return weatherData && weatherData.split(',').some(city => city.toLowerCase() === cityName.toLowerCase());
+}
 
-    msg.textContent = ''
-    msg.classList.remove('visible')
-
-    let inputVal = input.val()
-
-    const listItemsArray = Array.from(list.querySelectorAll('.cities li'))
-
-    if (listItemsArray.length > 0) {
-        const filteredArray = listItemsArray.filter(el => {
-            let content = ''
-            let cityName = el.querySelector('.city_name').textContent.toLowerCase()
-            let cityCountry = el.querySelector('.city_country').textContent.toLowerCase()
-
-            if (inputVal.includes(',')) {
-                if (inputVal.split(',')[1].length > 2) {
-                    inputVal = inputVal.split(',')[0]
-
-                    content = cityName
-                } else {
-                    content = `${cityName},${cityCountry}`
-                }
-            } else {
-                content = cityName
-            }
-            return content == inputVal.toLowerCase()
-        })
-
-        if (filteredArray.length > 0) {
-            msg.textContent = `You already know the weather for ${filteredArray[0].querySelector(".city_name").textContent}`;
-            msg.classList.add('visible')
-
-            form.trigger("reset")
-            input.focus()
-
-            return
-        }
+function addCityToTheBoard(cityName, initialCall, coords) {
+    let url;
+    
+    if (coords) {
+        url = `https://api.openweathermap.org/data/2.5/weather?lat=${coords.latitude}&lon=${coords.longitude}&appid=${APIkey}&units=metric`;
+    } else {
+        url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${APIkey}&units=metric`;
     }
-
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${APIkey}&units=metric`
 
     fetch(url)
         .then(response => response.json())
         .then(data => {
-
             if (data.cod == '404') {
-                throw new Error(`${data.cod}, ${data.message}`)
+                throw new Error(`${data.cod}, ${data.message}`);
             }
 
-            const { main, name, sys, weather } = data
+            const { main, name, sys, weather } = data;
 
-            const icon = `img/weather/${weather[0]['icon']}.png`
+            if (!initialCall) {
+                const updatedWeatherData = (localStorage.getItem('weather') ? localStorage.getItem('weather') + ',' : '') + name;
+                localStorage.setItem('weather', updatedWeatherData);
+            }
 
-            const li = document.createElement('li')
+            const icon = `img/weather/${weather[0]['icon']}.png`;
+            const li = document.createElement('li');
 
             const markup = `
                 <button><img src="./img/close.svg" alt="" class="close-button"></button>
@@ -75,18 +62,52 @@ form.submit(e => {
                     <img src="${icon}" alt="${weather[0]['description']}">
                 </figure>
                 <p class="city_conditions">${weather[0]['description'].toUpperCase()}</p>
-            `
-            li.innerHTML = markup
-            li.onclick = e => {
-                if (e.target.className === "close-button") li.remove()
-            }
-            list.appendChild(li)
+            `;
+            li.innerHTML = markup;
+
+            li.addEventListener('click', e => {
+                if (e.target.className === "close-button") {
+                    const weatherData = localStorage.getItem('weather');
+                    localStorage.setItem('weather', weatherData.split(',').filter(city => city !== name));
+                    li.remove();
+                }
+            })
+            list.appendChild(li);
         })
         .catch(() => {
-            msg.textContent = 'Please search for a valid city.'
-            msg.classList.add('visible')
+            msg.textContent = 'Please search for a valid city.';
+            msg.classList.add('visible');
         })
-    msg.textContent = ''
-    form.trigger("reset")
-    input.focus()
+}
+
+form.submit(e => {
+    e.preventDefault();
+
+    msg.textContent = '';
+    msg.classList.remove('visible');
+
+    let inputValue = input.val();
+    
+    const arrayOfCities = inputValue.split(',');
+    arrayOfCities.forEach(cityName => {
+        const city = cityName.trim();
+        if (checkIfCityIsAdded(city)) {
+            msg.textContent = `You already know the weather for ${city}`;
+            msg.classList.add('visible');
+
+            setTimeout(() => {
+                msg.classList.remove('visible');
+            }, 1000);
+    
+            form.trigger("reset");
+            input.focus();
+            return;
+        }
+
+        addCityToTheBoard(city);
+    });
+
+    
+    form.trigger("reset");
+    input.focus();
 })
