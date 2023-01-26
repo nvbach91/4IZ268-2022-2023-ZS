05f5146a80eaa0d92128c442885ccf326dbc850f
DariaKhari <113994017+DariaKhari@users.noreply.github.com>
2023-01-27 00:12:23 +0100

    Version 03
    changed based on comments

--- www/khad04/sp2/js/main.js
+++ www/khad04/sp2/js/main.js
@@ -1,73 +1,91 @@
-const API_KEY = 'lymvppQcIT5/lCWCAng6YA==UiiiNhc3PAs95IvM'
-
-const form = document.querySelector('#search_form')
-const input = document.querySelector('#search_term')
-const msg = document.querySelector(".form_msg")
-const list = document.querySelector(".cats")
-const container = document.querySelector('#container');
-
-
-function checkIfHasDuplicates(newBreed) {
-	const catsList = Array.from(list.querySelectorAll('.cats li'))
-	if (catsList.length > 0) {
-		return catsList.some(el => {
-			let oldBreed = el.querySelector('.breed_name').textContent
-			return oldBreed === newBreed
-		})
-	}
-}
-
+const form = document.querySelector('#search_form');
 
 form.addEventListener('submit', e => {
-	e.preventDefault()
+	e.preventDefault();
 
-	msg.textContent = ''
-	msg.classList.remove('visible')
+	const msg = document.querySelector(".form_msg");
+	msg.textContent = '';
+	msg.classList.remove('visible');
 
-	let inputValue = input.value.toLowerCase()
-	const url = `https://api.api-ninjas.com/v1/cats?name=${inputValue}`
+	const input = document.querySelector('#search_term');
+	let inputValue = input.value.toLowerCase();
+	addCatBreedToDisplay(inputValue);
+
+	msg.textContent = '';
+	form.reset();
+	input.focus();
+})
+
+const initialBreedsData = localStorage.getItem('catBreeds');
+if (initialBreedsData) {
+    initialBreedsData.split(',').forEach(breedName => {
+        addCatBreedToDisplay(breedName, true);
+    })
+}
+
+function checkIfHasDuplicates(newBreed) {
+	const breedsData = localStorage.getItem('catBreeds') ;
+    return breedsData && breedsData.split(',').some(breed => breed.toLowerCase() === newBreed.toLowerCase());
+}
+
+function addBreedToLocalStorage(newBreed) {
+	const updatedBreedsData = (localStorage.getItem('catBreeds') ? localStorage.getItem('catBreeds') + ',' : '') + newBreed;
+    localStorage.setItem('catBreeds', updatedBreedsData);
+}
+
+function addCatBreedToDisplay(breedName, initialLoad) {
+	const url = `https://api.api-ninjas.com/v1/cats?name=${breedName}`;
 	const options = {
 		headers: {
-			'X-Api-Key': API_KEY
+			'X-Api-Key': 'lymvppQcIT5/lCWCAng6YA==UiiiNhc3PAs95IvM'
 		}
 	}
+	const msg = document.querySelector(".form_msg");
+	const list = initialLoad ? document.querySelector(".initial_cats") : document.querySelector(".cats");
 
 	fetch(url, options)
 		.then(response => response.json())
 		.then(data => {
-			const { name, origin, max_weight: weight, image_link: imageSrc, intelligence } = data[0]
+			const { name, origin, max_weight: weight, image_link: imageSrc, intelligence } = data[0];
 
-			if (checkIfHasDuplicates(name)) {
-				msg.textContent = `You already have ${name} in your list`;
-				msg.classList.add('visible')
-				return
+			if (!initialLoad) {
+				if (checkIfHasDuplicates(name)) {
+					msg.textContent = `You already have ${name} in your list`;
+					msg.classList.add('visible');
+					return;
+				}
+				addBreedToLocalStorage(name);
 			}
-			const li = document.createElement('li')
+
+			const li = document.createElement('li');
 			const markup = `
-			<button><img src="./img/close.png" alt="" class="close-button"></button>
-			<h3 class="breed_name">${name}</h3>
-			<div class="info" >
-			<div class="center_cropped">
-				<img src="${imageSrc}" alt="${name}">
-			</div>
-			<div class="features" >Origin: ${origin} </br>  </br> Max weight: ${weight}kg </br>  </br> Intelligence: ${intelligence} </div>
-			</div>
-		`
-			li.innerHTML = markup
+				<button><img src="./img/close.png" alt="" class="close-button"></button>
+				<h3 class="breed_name">${name}</h3>
+				<div class="info" >
+					<div class="center_cropped">
+						<img src="${imageSrc}" alt="${name}">
+					</div>
+					<div class="features">
+						<div>Origin: ${origin}</div>
+						<div>Max weight: ${weight}kg</div>
+						<div>Intelligence: ${intelligence}</div>
+					</div>
+				</div>
+			`;
+			li.innerHTML = markup;
 
-			li.onclick = e => {
-				if (e.target.className === "close-button") li.remove()
-			}
+			li.addEventListener('click', e => {
+				if (e.target.className === "close-button") {
+					const breedsData = localStorage.getItem('catBreeds');
+                    localStorage.setItem('catBreeds', breedsData.split(',').filter(breedName => breedName !== name));
+					li.remove()
+				}
+			});
 
-			list.appendChild(li)
+			list.appendChild(li);
 		})
 		.catch(() => {
-			msg.textContent = 'Please search for a valid cat breed.'
-			msg.classList.add('visible')
+			msg.textContent = 'Please search for a valid cat breed.';
+			msg.classList.add('visible');
 		})
-
-	msg.textContent = ''
-	form.reset()
-	input.focus()
-})
-
+}
