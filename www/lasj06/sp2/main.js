930e15fd990d63eca85ff3d3fd3d5551422c2e93
Gladyon <46451552+Gladyon@users.noreply.github.com>
2023-01-30 21:23:59 +0100

    setting up ajax gets

--- www/lasj06/sp2/main.js
+++ www/lasj06/sp2/main.js
@@ -1,16 +1,57 @@
-function guess() {
-    const input = document.getElementById('input').value;
-    console.log(input);
-};
+var randomnumber = (Math.random() * (1008 - 1 + 1) ) << 0;
 
-function reset() {
-    console.log("reset");
-};
+$(document).ready(() => {
+    
+    $.ajax({
+        method: 'GET',
+        url: 'https://pokeapi.co/api/v2/pokemon/' + randomnumber,
+        dataType: 'json',
+    }).done(function(data){
+        const name = data.species.name;
+        const gen = "";
+        const type1 = data.types[0].type.name;
+        console.log(Object.keys(data.types).length)
+        if (Object.keys(data.types).length = 1) {
+            var type2 = "none";
+        } else {
+            var type2 = data.types[1].type.name;
+        };
+        const height = data.height;
+        const weight = data.weight;
+        resultData = [name, gen, type1, type2, height, weight]
+        console.log(resultData);
+    });
 
-function help() {
-    console.log("help");
-};
-
-function settings() {
-    console.log("settings");
-};
+    $("#button").click(function(){
+        const guessValue = document.getElementById('input').value;
+        
+        $.ajax({
+            method: 'GET',
+            url: 'https://pokeapi.co/api/v2/pokemon/' + guessValue,
+            dataType: 'json',
+        }).done(function(data){
+            const name = data.species.name;
+            const gen = "";
+            const type1 = data.types[0].type.name;
+            console.log(Object.keys(data.types).length)
+            if (Object.keys(data.types).length = 1) {
+                var type2 = "none";
+            } else {
+                var type2 = data.types[1].type.name;
+            };
+            const height = data.height;
+            const weight = data.weight;
+            guessData = [name, gen, type1, type2, height, weight]
+            console.log(guessData);
+        
+            if (guessData[0] == resultData[0]) {
+                console.log(true);
+            } else {
+                ;
+            }
+    
+        })
+        document.getElementById('input').value = '';
+    });
+    
+});
