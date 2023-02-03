d9408d06e825423a75c8d6e1e43ad52bda8f13c4
jakubrousal <114013170+jakubrousal@users.noreply.github.com>
2023-02-03 13:17:47 +0100

    version 03 - final
    Final version

--- www/rouj07/sp2/js/main.js
+++ www/rouj07/sp2/js/main.js
@@ -1,113 +1,165 @@
 $(document).ready(() => {
+    creating();
+    $("#searchForm").on("submit", (event) => {
+        let searchText = $("#searchText").val();
+        getMovies(searchText);
+        event.preventDefault();
+    });
+});
 
-    $('.search-button').click(setQuery);
-    const noName = 'You must enter a name of a movie or a tv series'
-    //function calls getMovie (if there is some input)
-    function setQuery() {
-        if ($('.search').val() != 0) {
-            getMovie($('.search').val());
-        } else {
-            $('.title').text(noName);
-            $('.poster').attr('src', '');
-            $('.genre').text('');
-            $('.released').text('');
-            $('.director').text('');
-            $('.writer').text('');
-            $('.actors').text('');
-            $('.imdb-rating').text('');
-            $('.plot').text('');
-        }
+const title = $('.title');
+const genre = $('.genre');
+const released = $('.released');
+const director = $('.director');
+const writer = $('.writer');
+const actors = $('.actors');
+const imdbRating = $('.imdb-rating');
+const plot = $('.plot');
+const input = $('.search');
+
+
+$('.search-button').click(setQuery);
+const noName = 'You must enter a name of a movie or a tv series'
+//function calls getMovie (if there is some input)
+function setQuery() {
+    if (input != 0) {
+        getMovies(input.val());
+    } else {
+        $('.title').text(noName);
+        $('.poster').attr('src', '');
+        $('.genre').text('');
+        $('.released').text('');
+        $('.director').text('');
+        $('.writer').text('');
+        $('.actors').text('');
+        $('.imdb-rating').text('');
+        $('.plot').text('');
     }
+}
 
-    const posterImg = $('<img class="poster">');
-    posterImg.appendTo('.img');
-
-    const notExistingMovie = 'I can\'t find this movie';
-
-    //axios gets info about searched movie
-    function getMovie(searchText) {
-        $('.overlay').show();
-        axios.get(`http://www.omdbapi.com/?i=tt3896198&apikey=70e522f5&t=${searchText}`).then((resp) => {
-            $('.overlay').hide();
-            const movie = resp.data;
-            $('.poster').attr('src', movie.Poster)
-            $('.title').text(movie.Title);
-            $('.genre').text(movie.Genre);
-            $('.released').text(movie.Released);
-            $('.director').text(movie.Director);
-            $('.writer').text(movie.Writer);
-            $('.actors').text(movie.Actors);
-            $('.imdb-rating').text(movie.imdbRating);
-            $('.plot').text(movie.Plot);
-
-            sessionStorage.setItem('title', movie.Title);
-
-            if (movie.Poster === 'N/A') {
-                $('.poster').attr('src', 'https://charbase.com/images/glyph/65110');
-            }
-
-            if (resp.data.Error) {
-                console.log("no data found");
-                $('.title').text(notExistingMovie);
-                $('.poster').attr('src', '')
-                $('.genre').text('');
-                $('.released').text('');
-                $('.director').text('');
-                $('.writer').text('');
-                $('.actors').text('');
-                $('.imdb-rating').text('');
-                $('.plot').text('');
-            }
+const overlay = $('.overlay');
+const container = $('.container');
+const particular = $('.particular');
+function getMovies(searchText) {
+    overlay.show();
+    container.show()
+    particular.hide()
+    axios
+        .get(`http://www.omdbapi.com/?i=tt3896198&apikey=70e522f5&s=${searchText}`)
+        .then((response) => {
+            overlay.hide();
+            let movies = response.data.Search;
+            let output = "";
+            $.each(movies, (index, movie) => {
+                output += `
+                    <div class="item">
+                    <img src="${movie.Poster}"/>
+                            <h4>${movie.Title}</h4>
+                            <div>${movie.Year}</div>
+                            <button onclick="movieSelected('${movie.imdbID}')"class="btn">Movie Details</button>
+                    </div>`;
+            });
+            $('.output').html(output);
         })
-            .catch((err) => {
-                console.log(err);
-            })
+        .catch((error) => {
+            console.log(error);
+        });
 
-        $('.save-button').click(save);
+}
 
-        const alreadySaved = 'You already saved this movie'
+function movieSelected(id) {
+    sessionStorage.setItem('key', id);
+    console.log(sessionStorage.getItem('key'));
+    getMovie();
 
-        //save-button.click can save a movie to local storage
-        function save() {
-            for (let i = 0; i < localStorage.length; i++) {
-                if (localStorage.getItem(localStorage.key(i)).includes($('.title').text())) {
-                    $('.title').text(alreadySaved)
-                    $('.poster').attr('src', '')
-                    $('.genre').text('');
-                    $('.released').text('');
-                    $('.director').text('');
-                    $('.writer').text('');
-                    $('.actors').text('');
-                    $('.imdb-rating').text('');
-                    $('.plot').text('');
-                }
-            }
-            let searchedText = sessionStorage.getItem('title');
-            let key = searchedText.split(',', 1);
-            if (searchedText.length != 0 && !$('.title').text().includes(notExistingMovie) && !$('.title').text().includes(noName) && !$('.title').text().includes(alreadySaved)) {
-                localStorage.setItem(key, searchedText);
-                location.reload();
-            }
+}
+
+const notExistingMovie = 'I can\'t find this movie';
+
+//axios gets info about searched movie
+function getMovie() {
+    let movieId = sessionStorage.getItem('key');
+    overlay.show();
+    axios.get(`http://www.omdbapi.com/?i=${movieId}&apikey=70e522f5`).then((resp) => {
+        overlay.hide();
+        const movie = resp.data;
+        const posterImg = $('<img class="poster">');
+        posterImg.appendTo('.img');
+        const poster = $('.poster');
+        poster.attr('src', movie.Poster);
+        poster.attr('alt', '');
+        title.text(movie.Title);
+        genre.text(movie.Genre);
+        released.text(movie.Released);
+        director.text(movie.Director);
+        writer.text(movie.Writer);
+        actors.text(movie.Actors);
+        imdbRating.text(movie.imdbRating);
+        plot.text(movie.Plot);
+        container.hide()
+        particular.show();
+
+        sessionStorage.setItem('title', movie.Title);
+
+        if (movie.Poster === 'N/A') {
+            poster.attr('src', 'https://charbase.com/images/glyph/65110');
         }
-    }
 
+        if (resp.data.Error) {
+            console.log("no data found");
+            title.text(notExistingMovie);
+            poster.attr('src', '');
+            genre.text('');
+            released.text('');
+            director.text('');
+            writer.text('');
+            actors.text('');
+            imdbRating.text('');
+            plot.text('');
+        }
+    })
+        .catch((err) => {
+            console.log(err);
+        })
+
+    $('.save-button').click(save);
+
+    const alreadySaved = 'You already saved this movie'
+
+    //save-button.click can save a movie to local storage
+    function save() {
+        movies.empty()
+        buttons.empty()
+        let searchedText = sessionStorage.getItem('title');
+        let key = searchedText.split(',', 1);
+        localStorage.setItem(key, searchedText);
+        creating();
+
+    }
+}
+
+const movies = $('.movies');
+const buttons = $('.buttons');
+const caption = $('.caption');
+//creates text and buttons for saved movies 
+function creating() {
     const text = [];
     const bttns = [];
-
-    //creates text and buttons for saved movies 
     for (let i = 0; i < localStorage.length; i++) {
         let key = localStorage.key(i);
+        console.log(key)
         let value = localStorage.getItem(key);
+        console.log(value)
         let split = value.split(',', 1);
-
-        const movieText = $('<p>')
+        console.log(split)
+        const movieText = $('<p class="text">')
         movieText.text(`${value}`);
         text.push(movieText);
 
-        const loadButton = $('<button>&#10003;</button>')
+        const loadButton = $('<button class="button">&#10003;</button>')
         bttns.push(loadButton);
 
-        const deleteButton = $('<button>&#10006;</button>')
+        const deleteButton = $('<button class="button">&#10006;</button>')
         bttns.push(deleteButton);
 
         const deleteAllButton = $('<button class="delete-all">Delete all</button>');
@@ -137,13 +189,17 @@ $(document).ready(() => {
 
         function deleteAll() {
             localStorage.clear();
-            $('.movies').remove();
-            $('.buttons').remove();
-            $('.caption').show();
+            movies.remove();
+            buttons.remove();
+            caption.show();
         }
-        $('.caption').hide();
+        caption.hide();
     }
-    $('.movies').append(...text);
-    $('.buttons').append(...bttns);
 
-});
+
+    movies.append(...text);
+    buttons.append(...bttns);
+
+}
+
+
